14a5655e1760414ce3172adb69b7abf9b5226663
Shiv <167079300+Shiv-hcr@users.noreply.github.com>
2026-05-13 14:25:38 +0100

    Create hash.test.ts

---
+++ tests/internals/hash.test.ts
@@ -0,0 +1,15 @@
+import * as B from "bun:test";
+import { murmur3_32 } from "../../src/internals/hash";
+
+const wrapper = (item: string, seed?: number) => murmur3_32(Buffer.from(item), seed);
+
+B.describe(murmur3_32, () => {
+    B.test("expect values to match Bun impl", () => {
+        const input = "Hello World!";
+        const seed = Math.floor(Math.random() * 1_000);
+        const h1 = Bun.hash.murmur32v3(input, seed);
+        const h2 = wrapper(input, seed);
+
+        B.expect(h2).toBe(h1);
+    }, { repeats: 1_000 });
+});
