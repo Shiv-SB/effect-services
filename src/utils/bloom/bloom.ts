281bf7235ed855b106976252aa8fe6f899ed297b
Shiv <167079300+Shiv-hcr@users.noreply.github.com>
2026-05-13 14:08:28 +0100

    jsdoc + non-bun impl

--- src/utils/bloom/bloom.ts
+++ src/utils/bloom/bloom.ts
@@ -1,8 +1,9 @@
 import type { BloomFilterImpl, BloomFilterArgs } from "./common";
+import { murmur3_32 } from '../../internals/hash';
 
 const SizeOfBitArray = (
-    n: number,
-    p: number,
+    n: number,  // expected no. of items
+    p: number,  // false positive probability (0 - 1)
 ): number => {
     const upper = n * Math.log(p) * -1;
     const lower = Math.LN2 ** 2;
@@ -10,10 +11,33 @@ const SizeOfBitArray = (
 };
 
 const OptimumNoOfHashFuncs = (
-    m: number,
-    n: number,
+    m: number,  // bit array size
+    n: number,  // expected no. of items
 ): number => Math.max(1, Math.ceil((m * Math.LN2) / n));
 
+type HashFunction = (item: string, seed: number) => number;
+
+/**
+ * Probabilistic Bloom filter implementation.
+ *
+ * A Bloom filter is a space-efficient probabilistic data structure used to test
+ * whether an element is a member of a set.
+ *
+ * If the actual item count exceeds the given `item_count`, the accuracy of the
+ * filter will be dramatically reduced.
+ * Consider using the `ScalableBloomFilter` for situations where
+ * the initial item count is unknown.
+ * 
+ * Characteristics:
+ * - False positives are possible.
+ * - False negatives are not possible.
+ * - Insert-only structure (no deletion support).
+ *
+ * This implementation uses:
+ * - Double hashing for digest generation.
+ * - The hashing algorithm used is Murmer V3 32 bit.
+ * - A compact bit-packed buffer instead of an Array for storage.
+ */
 export class BloomFilter implements BloomFilterImpl {
     private capacity: number;
     private inserted = 0;
@@ -22,7 +46,7 @@ export class BloomFilter implements BloomFilterImpl {
     private hash_count: number;
 
     private bits: Buffer<ArrayBuffer>;
-    private hasher = Bun.hash.murmur32v3;
+    private hasher: HashFunction;
 
     constructor(args: BloomFilterArgs) {
         const {
@@ -42,6 +66,10 @@ export class BloomFilter implements BloomFilterImpl {
 
         this.bits = Buffer.alloc(byteSize, 0);
 
+        this.hasher = process.versions.bun
+            ? Bun.hash.murmur32v3
+            : (item, seed) => murmur3_32(Buffer.from(item), seed);
+
         /*console.log({
             capacity: this.capacity,
             fp_prob: this.fp_prob,
@@ -51,13 +79,32 @@ export class BloomFilter implements BloomFilterImpl {
         });*/
     }
 
+    /**
+     * Determines whether the filter has reached its configured capacity.
+     *
+     * Note:
+     * Bloom filters can technically continue accepting inserts beyond capacity,
+     * but false positive probability will increase beyond the configured target.
+     *
+     * @returns `true` if the configured capacity has been reached.
+    */
     public isAtCapacity = (): boolean =>
         this.inserted >= this.capacity;
 
+    /**
+     * Gets the number of inserted items.
+     *
+     * @returns Current inserted item count.
+    */
     public get currentSize() {
         return this.inserted;
     }
 
+    /**
+     * Gets the configured item capacity for the Bloom filter.
+     *
+     * @returns Maximum expected insert count.
+    */
     public getCapacity = (): number =>
         this.capacity;
 
--- src/utils/bloom/common.ts
+++ src/utils/bloom/common.ts
@@ -1,12 +1,41 @@
 //#region Types & Classes
 
 export interface BloomFilterArgs {
+    /**
+     * Expected maximum number of inserted items.
+    */
+   
     item_count: number;
+    /**
+     * Desired false positive probabilty
+     * 
+     * Must be a value between 0 and 1.
+     *@default 0.01
+    */
     fp_prob?: number;
 }
 
 export interface BloomFilterImpl {
+    /**
+     * Inserts an item into the Bloom filter.
+     *
+     * Multiple hash digests are generated for the item and corresponding bits
+     * are marked in the backing bit array.
+     *
+     * @param item Item to insert.
+    */
     insert: (item: string) => void;
+
+    /**
+     * Checks whether an item may exist in the Bloom filter.
+     *
+     * Results:
+     * - `false` means the item definitely does not exist.
+     * - `true` means the item may exist.
+     *
+     * @param item Item to test.
+     * @returns Membership probability result.
+     */
     has: (item: string) => boolean;
 }
 
